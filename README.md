2a45b42894f407356659fca94e34cfbebddeda43
Carl Velasco <carlvelasco96@gmail.com>
2022-05-01 21:24:22 +1200

    Basic Map App Completed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# learn-typescript
- 
+# Setup
+
+Run TypeScript in the browser using Parcel
+
+To install, run: `npm install -g parcel`.
+
+To run the application, run: `parcel index.html` or `npm start`.
---
+++ src/CustomMap.ts
@@ -0,0 +1,72 @@
+/**
+ * The purpose of this map is to limit the methods that our development team can access
+ * to prevent breaking of our codebase.
+ */
+
+import { User } from "./User";
+import { Company } from "./Company";
+
+// All input to addMarker needs properties defined here
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      }
+    });
+  }
+  
+  // With the OR operator for TypeScript, the autocomplete feature will only account for
+  // properties that are shared between the two interfaces.
+  // public addMarker(mappable: User | Company): void {
+  public addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+
+  // public addUserMarker(user: User): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng
+  //     }
+  //   });
+  // }
+
+  // public addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   });
+  // }
+}
